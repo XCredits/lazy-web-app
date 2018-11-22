112866806f0338ea8c7d2c11f1361ce9cb4f18bc
AS <misstechspec@gmail.com>
2018-11-22 14:54:51 +1100

    Add db for testing

---
+++ db.ts
@@ -0,0 +1,16 @@
+import * as bluebird from 'bluebird';
+import * as sqlite3Callback from 'sqlite3';
+
+const sqlite3 = bluebird.Promise.promisifyAll(sqlite3Callback.verbose());
+
+export function dbSetup () {
+  const connection: any = new sqlite3.Database('databasename');
+  connection.runAsync('CREATE TABLE if not exists dummy_table (info TEXT)');
+  connection.runAsync('INSERT into dummy_table(col1,col2,col3) VALUES (apple,pear,orange)');
+  //const tblValue = connection.prepare("INSERT INTO dummy_table VALUES (?)");
+  let data = connection.get('SELECT * FROM dummy_table');
+  console.log(data);
+  // for (var i = 0, , i++) {
+  //   tblValue.runAsync('Table data ' + i);
+  // }
+}
