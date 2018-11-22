a3467088787eadc1875ca84ea1aa7e009e0d6406
AS <misstechspec@gmail.com>
2018-11-22 15:47:49 +1100

    change db ts to js

--- server/db.js
+++ server/db.js
@@ -1,10 +1,13 @@
-import * as bluebird from 'bluebird';
-import * as sqlite3Callback from 'sqlite3';
+const bluebird = require('bluebird');
+const sqlite3Callback = require('sqlite3');
+
+// import * as bluebird from 'bluebird';
+// import * as sqlite3Callback from 'sqlite3';
 
 const sqlite3 = bluebird.Promise.promisifyAll(sqlite3Callback.verbose());
 
-export function dbSetup () {
-  const connection: any = new sqlite3.Database('databasename');
+module.exports = function dbSetup() {
+  const connection = new sqlite3.Database('databasename');
   connection.runAsync('CREATE TABLE if not exists dummy_table (info TEXT)');
   connection.runAsync('INSERT into dummy_table(col1,col2,col3) VALUES (apple,pear,orange)');
   //const tblValue = connection.prepare("INSERT INTO dummy_table VALUES (?)");
@@ -13,4 +16,4 @@ export function dbSetup () {
   // for (var i = 0, , i++) {
   //   tblValue.runAsync('Table data ' + i);
   // }
-}
+};
