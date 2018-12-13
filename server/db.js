45d5d3f6ea346c28c57dac659b29a1ffeafafbf3
AS <misstechspec@gmail.com>
2018-12-14 10:01:19 +1100

    Add db functions with variable items and table names

--- server/db.js
+++ server/db.js
@@ -1,6 +1,9 @@
 const Promise = require('bluebird');  
 const DatabaseService = require('./services/database.service');
-const ProductModel = require('./models/product.model');
+const ProductModel = require('./models/product.model'); 
+const GenericItemModel = require('./models/generic-item.model'); 
+const sqlite3 = require('sqlite3');
+//const Promise = require('util');  
 
 function productDbDemo() {
     //instantiate data service object
@@ -41,7 +44,214 @@ function productDbDemo() {
                 console.log('Error:');
                 console.dir(err);
             })
-        );    
+        );
 }
 
-productDbDemo();
+//productDbDemo();
+
+function genericDbDemo() {
+    //instantiate data service object
+    const databaseService = new DatabaseService('./database.sqlite3');
+    //instantiate product object
+    const genericModel = new GenericItemModel(databaseService);
+
+    //create table
+    genericModel.createTable('TableA')
+        .then(() => {
+            //create array of items to add to table
+            const items = [
+                {
+                    name: 'ItemA',
+                },
+                {
+                    name: 'ItemB',
+                },
+            ];
+            //add array of items to table
+            return new Promise.all(items.map((item) => {
+                const {name} = item;
+                return genericModel.setFunction('TableA', name);
+            }));
+        }) //get all items from table and console log items
+        .then(() => genericModel.getAll('TableA')
+        .then((items) => {
+            console.dir(items);
+            return new Promise((resolve, reject) => {
+                items.forEach(function(item) {
+                    console.log(`Product Id: ${item.id}`);
+                    console.log(`Product Name: ${item.name}`);
+                });
+            }); //if successful display success
+            resolve('Success');     
+            }) //if error console log error
+            .catch((err) => {
+                console.log('Error:');
+                console.dir(err);
+            })
+        );
+}
+
+genericDbDemo();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const dbFilePath = 'xcreditsDb.sqlite';
+var dbConnection;
+//var dbConnection = new sqlite3.Database(dbFilePath);
+
+
+//open database
+const openDatabase = (dbFilePath) => {
+    dbConnection = new sqlite3.Database(dbFilePath, (err) => {
+        if (err) {
+        console.log('From Expressjs: Could not connect to database', err);
+        } else {
+        console.log('From Expressjs: Connected to database');
+        };  
+    });
+}
+
+//promisify open database function
+const openDatabaseAsync = Promise.promisify(openDatabase);
+
+//call open database function
+openDatabaseAsync(dbFilePath) 
+    .then((msg) => console.log(`Success msg: ${msg}`))
+    .catch((err) => console.log(`Error: ${err}`));
+
+  //create table 
+  function createTable() {
+    //const dbConnectionRun = Promise.promisify(dbConnection.run);
+    return dbConnection.run(
+      `CREATE TABLE IF NOT EXISTS genericTable (
+        keyPath INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        password TEXT
+      )`,
+      function (err) {
+        if (err) {
+          console.log(`Error: ${err}`);
+        } else {
+          console.log(`Success: created table`)
+        }
+      }
+    )
+  }
+
+  //promisify create table function
+  const createTableAsync = Promise.promisify(createTable);
+
+  //delete table
+  function deleteTable(tableName) {
+    dbConnection.run(
+      `DROP TABLE IF EXISTS genericTable`,
+      [tableName],
+      function (err) {
+        if (err) {
+          console.log(`Error: ${err}`);
+        } else {
+          console.log(`Success: deleted table`)
+        }
+      }
+    )
+  }
+
+  //promisify delete table function
+  const deleteTableAsync = Promise.promisify(deleteTable);
+
+  //add entry to table
+  function setFunction(name, password) {
+
+    const dbConnectionRunAsync = Promise.promisify(dbConnection.run);
+    return dbConnectionRunAsync(
+        'INSERT INTO genericTable (name, password) VALUES (?, ?)',
+        [name, password])
+        .then((res) => {
+            return console.log(res);
+            console.log('Added to table');
+            //res = console.log('Add to table');
+            // res1 = res;
+            // return res1;
+        })
+        .catch((error) => {
+            retutnconsole.log(error);
+            console.log('Error adding to table');
+            //return console.log('Error adding to table');
+        });  
+  };
+
+  var name = 'John';
+  var password = 'password1';
+
+  //setFunction(name, password);
+    // .then((msg) = console.log('Success'))
+    // .catch((err) = console.log('Error'));
+  
+  //promisify set Function function
+  //const setFunctionAsync = Promise.promisify(setFunction);
+
+  function getAllFunction() {
+    dbConnection.all(
+      `SELECT * FROM genericTable`,
+      function(err, rows) {
+        if (err) {
+          console.log(`Error: ${err}`);
+        } else {
+          console.log(`Success: got all items from table`);
+          console.log(rows);
+          // rows.array.forEach((row: any) => {
+          //   console.log(row.keyPath);
+          //   console.log(row.name);
+          //   console.log(row.obj);
+          // });
+        }
+      }
+    )
+  }
+
+  //promisify set Function function
+  const getAllFunctionAsync = Promise.promisify(getAllFunction);
+
+  function getFunction() {
+    dbConnection.all(
+      `SELECT * FROM genericTable`,
+      function(err, rows) {
+        if (err) {
+          console.log(`Error: ${err}`);
+        } else {
+          console.log(`Success: got all items from table`);
+          console.log(rows);
+          // rows.array.forEach((row: any) => {
+          //   console.log(row.keyPath);
+          //   console.log(row.name);
+          //   console.log(row.obj);
+          // });
+        }
+      }
+    )
+  }
+
+//createTableAsync()
+ //  .then(()=>{
+ //    return getAllFunction   });
+
+//var name = 'John';
+//var password = 'password1';
+//setFunction(name, password);
+    // .then((msg) = console.log('Success'))
+    // .catch((err) = console.log('Error'));
+
+  //getAllFunctionAsync();
---
+++ server/models/generic-item.model.js
@@ -0,0 +1,69 @@
+class GenericItemSchema {
+  //constructor
+  constructor(databaseService) {
+    this.databaseService = databaseService;
+  }; 
+
+  //create table method
+  createTable(tableName) {
+    const sql = `
+    CREATE TABLE IF NOT EXISTS ${tableName} (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT)`;
+    return this.databaseService.run(sql);
+  };
+
+  //create table method
+  deleteTable(tableName) {
+    const sql = `
+    DROP TABLE IF EXISTS ${tableName}`;
+    return this.databaseService.run(sql);
+  };
+
+  //add item to table method
+  setFunction(tableName, name) {
+    return this.databaseService.run(
+      `INSERT INTO ${tableName} (name) VALUES (?)`,
+      [name]);
+  };
+
+  //update item in table
+  updateFunction(tableName, item) {
+    const { id, name } = item;
+    return this.databaseService.run(
+      `UPDATE ${tableName} SET name = ? WHERE id = ?`,
+      [name, id]
+    );
+  };
+
+  //delete item in table
+  deleteFunction(tableName, id) {
+    return this.databaseService.run(
+      `DELETE FROM ${tableName} WHERE id = ?`,
+      [id]
+    );
+  };
+
+  //get item in table by id
+  getFunction(tableName, id) {
+    return this.databaseService.get(
+      `SELECT * FROM ${tableName} WHERE id = ?`,
+      [id]);
+  };
+
+  //UNFINISHED
+  //get item in table by id
+  getRangeFunction(tableName, id) {
+    return this.databaseService.get(
+      `SELECT * FROM ${tableName} WHERE id = ?`,
+      [id]);
+  };
+
+  //get all items in table
+  getAll(tableName) {
+    return this.databaseService.all(`SELECT * FROM ${tableName}`)
+  };
+  
+}
+
+module.exports = GenericItemSchema;
