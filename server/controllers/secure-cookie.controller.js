ede3e2af3527d0960e580ad56f2382ece5d266b5
James Jansson <jamesjansson@gmail.com>
2018-12-23 02:08:27 +1100

    Move out code into libraries

---
+++ server/controllers/secure-cookie.controller.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = function(req, res, next) {
+  if (req.hostname !== 'localhost') {
+    let cookieFunction = res.cookie;
+    res.cookie = function(name, value, options) {
+      let newOptions = {};
+      if (typeof options !== 'undefined') {
+        newOptions = _.cloneDeep(options);
+      }
+      if (typeof newOptions.secure === 'undefined') {
+        newOptions.secure = true;
+      }
+      return cookieFunction.call(res, name, value, newOptions);
+    };
+  }
+  return next();
+};
