af543486b75607fe1ac13c78be2ece092c57dc44
James Jansson <jamesjansson@gmail.com>
2018-12-31 01:14:10 +1100

    Rename

--- server/controllers/utils.controller.js
+++ server/controllers/utils.controller.js
@@ -1,14 +1,37 @@
-const usernameRegex = /^[a-zA-Z0-9_.-]*$/;
+const displayUsernameRegex = /^[a-zA-Z0-9_.-]*$/;
+const usernameRegex = /^[a-z0-9]*$/;
 
 module.exports = {
   isValidUsername,
-}
+  isValidDisplayUsername,
+  convertToDisplayUsername,
+};
 
+/**
+ *
+ * @param {string} username
+ * @return {boolean}
+ */
 function isValidUsername(username) {
   return usernameRegex.test(username);
 }
 
-function stripUsername(username) {
+/**
+ *
+ * @param {string} username
+ * @return {boolean}
+ */
+function isValidDisplayUsername(username) {
+  return displayUsernameRegex.test(username);
+}
+
+
+/**
+ *
+ * @param {string} username
+ * @return {string}
+ */
+function convertToDisplayUsername(username) {
   return username
       .split('.').join('')
       .split('_').join('')
