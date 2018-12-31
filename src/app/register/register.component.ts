ce6e8f651a6fc54158677a6bb3feeb9d5c97be2b
James Jansson <jamesjansson@gmail.com>
2018-12-31 17:02:43 +1100

    Validation works

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, ValidatorFn, ValidationErrors, AbstractControl } from '@angular/forms';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { UserService, passwordLength } from '../user.service';
 import { AnalyticsService } from '../analytics.service';
-import { ValidateZxcvbnDirective } from './../validate-zxcvbn.directive';
+// import { ValidateZxcvbnDirective } from './../validate-zxcvbn.directive';
+
+import * as zxcvbn from 'zxcvbn';
+
+const minGuessesLog10 = 9;
+// const zxcvbnValidate: ValidatorFn = function (control: FormGroup): ValidationErrors | null {
+//   console.log('Ay ' + control);
+//   const password = control.get('password');
+//   if (zxcvbn(password.value).guesses_log10 > minGuessesLog10) { // sufficiently hard
+//     return null;
+//   } else {
+//     const a:  ValidationErrors = {
+//       'guesses_log10': {value: zxcvbn(password.value).guesses_log10}
+//     };
+//     return a;
+//   }
+// };
+
+const zxcvbnValidate: ValidatorFn = function (control: AbstractControl): ValidationErrors | null {
+  console.log('Ay ' + control);
+  const password = control.value;
+  if (zxcvbn(password).guesses_log10 > minGuessesLog10) { // sufficiently hard
+    return null;
+  } else {
+    const a:  ValidationErrors = {
+      'guesses_log10': {value: zxcvbn(password).guesses_log10}
+    };
+    return a;
+  }
+};
+
 
 @Component({
   selector: 'app-register',
@@ -23,7 +53,8 @@ export class RegisterComponent implements OnInit {
       private userService: UserService,
       private activatedRoute: ActivatedRoute,
       private analytics: AnalyticsService,
-      private zxcvbnValidator:  ValidateZxcvbnDirective) {
+      // private zxcvbnValidator:  ValidateZxcvbnDirective
+      ) {
 
         // The following is to ensure that when a user is redirected for the
         // purposes of logging in that they are still redirected to the correct
@@ -43,7 +74,7 @@ export class RegisterComponent implements OnInit {
           Validators.pattern(this.userService.displayUsernameRegexString)]),
       password: new FormControl('', [Validators.required,
           Validators.minLength(passwordLength),
-          this.zxcvbnValidator.validate,
+          zxcvbnValidate,
         ]),
     });
   }
--- src/app/validate-zxcvbn.directive.ts
+++ src/app/validate-zxcvbn.directive.ts
@@ -1,25 +1,37 @@
 import { Directive } from '@angular/core';
 import * as zxcvbn from 'zxcvbn';
-import { ValidatorFn,  ValidationErrors, AbstractControl } from '@angular/forms';
+import { ValidatorFn,  ValidationErrors, AbstractControl, NG_VALIDATORS } from '@angular/forms';
 
 const minGuessesLog10 = 8;
 
 @Directive({
-  selector: '[appValidateZxcvbn]'
+  selector: '[appValidateZxcvbn]',
+  providers: [{provide: NG_VALIDATORS, useExisting: ValidateZxcvbnDirective, multi: true}]
 })
 export class ValidateZxcvbnDirective {
   constructor() { }
 
-  validate(): ValidatorFn {
-    return function(control: AbstractControl) {
-      if (zxcvbn(control.value).guesses_log10 > minGuessesLog10) { // sufficiently hard
-        return null;
-      } else {
-        const a:  ValidationErrors = {
-          'guesses_log10': {value: zxcvbn(control.value).guesses_log10}
-        };
-        return a;
-      }
+  validate(control: AbstractControl) {
+    if (zxcvbn(control.value).guesses_log10 > minGuessesLog10) { // sufficiently hard
+      return null;
+    } else {
+      const a:  ValidationErrors = {
+        'guesses_log10': {value: zxcvbn(control.value).guesses_log10}
+      };
+      return a;
     }
   }
+
+  // validate(): ValidatorFn {
+  //   return function(control: AbstractControl) {
+  //     if (zxcvbn(control.value).guesses_log10 > minGuessesLog10) { // sufficiently hard
+  //       return null;
+  //     } else {
+  //       const a:  ValidationErrors = {
+  //         'guesses_log10': {value: zxcvbn(control.value).guesses_log10}
+  //       };
+  //       return a;
+  //     }
+  //   }
+  // }
 }
