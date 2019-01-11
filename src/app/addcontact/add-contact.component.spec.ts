1f1fa633cf8f4ed8bcf5be714c699c2c63c9ce0c
Hasan <swehu1988@gmail.com>
2019-01-11 15:22:18 +1100

    Modifying 'add-contact' component

--- src/app/addcontact/add-contact.component.spec.ts
+++ src/app/addcontact/add-contact.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MailingListComponent } from './add-contact';
+import { MailingListComponent } from './add-contact.components';
 
 describe('MailingListComponent', () => {
   let component: MailingListComponent;
