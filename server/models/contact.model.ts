f36446669677d1f302f70cfb9f97201017cf7a2f
Hasan <swehu1988@gmail.com>
2019-01-22 12:54:30 +1100

    Contact model modified

--- server/models/contact.model.ts
+++ server/models/contact.model.ts
@@ -15,20 +15,6 @@ ContactSchema.index({username: 1});
 ContactSchema.index({displayUsername: 1});
 ContactSchema.index({email: 1});
 
-
-ContactSchema.methods.createPasswordHash = function(password) {
-  // https://hackernoon.com/your-node-js-authentication-tutorial-is-wrong-f1a3bf831a46
-  // https://codahale.com/how-to-safely-store-a-password/
-  // https://security.stackexchange.com/questions/4781/do-any-security-experts-recommend-bcrypt-for-password-storage/6415#6415
-
-  this.saltRounds = 12;
-  this.passwordHash = bcrypt.hashSync(password, this.saltRounds);
-};
-
-ContactSchema.methods.checkPassword = function(password) {
-  return bcrypt.compareSync(password, this.passwordHash);
-};
-
 /**
  * removes secret data we don't want to send to the front-end
  * @return {*}
