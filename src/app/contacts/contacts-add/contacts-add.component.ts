efd6326ec75055d6ecb6ac88bc6ce3cb78ca6096
Hasan <swehu1988@gmail.com>
2019-03-19 15:53:15 +1100

    Contacts
    - Add multiple groups

--- src/app/contacts/contacts-add/contacts-add.component.ts
+++ src/app/contacts/contacts-add/contacts-add.component.ts
@@ -14,11 +14,9 @@ export class ContactsAddComponent implements OnInit {
   formErrorMessage: string;
   isEditMode: boolean;
   waiting: boolean;
-  rendered: boolean;
   groups: { groupId: string, groupName: string, numberOfContacts: number }[] = [];
-  toppings = new FormControl();
-  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
-  toppingSelection = [];
+  groupForm = new FormControl();
+  groupsSelection = [];
 
 
   constructor(
@@ -40,15 +38,10 @@ export class ContactsAddComponent implements OnInit {
 
   onSelection(selection) {
 
-    this.rendered = false;
-    console.log(selection);
-    console.log(selection.length);
-    this.toppingSelection = [];
-    for (const sele of selection) {
-      this.toppingSelection.push(sele);
+    this.groupsSelection = [];
+    for (const groupSelect of selection) {
+      this.groupsSelection.push(groupSelect);
     }
-    console.log(this.toppingSelection.length);
-    this.rendered = true;
   }
 
   loadContactsGroups = function () {
