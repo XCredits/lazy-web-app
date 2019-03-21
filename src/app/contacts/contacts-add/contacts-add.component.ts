042203ed7b54c6c3c5384cbb235b134dcbd3aeaa
Hasan <swehu1988@gmail.com>
2019-03-22 09:11:46 +1100

    Contact
    groups validation

--- src/app/contacts/contacts-add/contacts-add.component.ts
+++ src/app/contacts/contacts-add/contacts-add.component.ts
@@ -6,6 +6,7 @@ import {MatSnackBar, MatAutocompleteSelectedEvent, MatChipInputEvent, MatAutocom
 import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
 import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import { ContactsGroupDetailsComponent } from '../contacts-group/contacts-group-details/contacts-group-details.component';
 
 @Component({
   selector: 'app-contacts-add',
@@ -123,6 +124,11 @@ export class ContactsAddComponent implements OnInit {
   }
 
   add(event: MatChipInputEvent): void {
+    if (this.groups.indexOf( String(event.value) ) === -1) {
+      return;
+    }
+    console.log(event.value);
+    console.log(event.input);
     // Add group only when MatAutocomplete is not open
     // To make sure this does not conflict with OptionSelected Event
     if (!this.matAutocomplete.isOpen) {
